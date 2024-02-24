856f54626b64b1568178255b598610d1c3fa26a8
harsh4786 <50767810+harsh4786@users.noreply.github.com>
2024-02-24 16:17:02 +0530

    fix: patch all the dependency conflicts for v1.18.2

--- ellipsis-client/src/lib.rs
+++ ellipsis-client/src/lib.rs
@@ -4,6 +4,7 @@ pub mod banks_server;
 pub mod ellipsis_client;
 pub mod grpc_client;
 pub mod programs;
+pub mod program_test;
 
 #[macro_use]
 extern crate solana_bpf_loader_program;
@@ -11,20 +12,3 @@ extern crate solana_bpf_loader_program;
 pub use ellipsis_client::*;
 pub use ellipsis_transaction_utils as transaction_utils;
 
-/// Converts a `solana-program`-style entrypoint into the runtime's entrypoint style, for
-/// use with `ProgramTest::add_program`
-#[macro_export]
-macro_rules! processor {
-    ($process_instruction:expr) => {
-        Some(
-            |first_instruction_account: usize,
-             invoke_context: &mut solana_test_client::program_test::InvokeContext| {
-                $crate::program_test::builtin_process_instruction(
-                    $process_instruction,
-                    first_instruction_account,
-                    invoke_context,
-                )
-            },
-        )
-    };
-}
